b8939fbe05afcb2e3a6b2d3fbb2cac1d28f6dc2d
NycolasSF <nycolassilvafroes@gmail.com>
2019-01-27 00:03:44 -0300

    arrumando map e detalhes

---
+++ 01_SERVER/public/js/menu_mobile.js
@@ -0,0 +1,28 @@
+class Media_screen{
+  constructor(width) {
+    this.width = window.matchMedia("(min-width: "+width+")");
+  }
+  responsive(){
+    let mobile;
+    if (this.width.matches) {
+      return mobile = true;
+    } else {
+      return mobile = false;
+    }
+  }
+}
+let menu_screen = new Media_screen('940px');
+let icon = document.querySelector('.menu-mobile');
+
+  if (menu_screen) {
+    icon[0].className = 'mobile';
+  }else{
+    icon[0].className = 'desktop'
+  }
+
+function openNav() {
+  document.getElementsByTagName('nav')[0].style.display = "block";
+}
+function closeNav() {
+  document.getElementsByTagName('nav')[0].style.display = "none";
+}
