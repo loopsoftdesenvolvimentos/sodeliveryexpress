21d98a989c39c988f717a25f746389eedfef431f
marcos lopes <marcoslopes5687@gmail.com>
2019-01-31 18:17:24 -0300

    ALterações na paginas

--- 01_SERVER/public/js/pesquisa.js
+++
@@ -1,9 +0,0 @@
-
-function openPesquisa() {
-    document.getElementById("myPesquisa").style.width = "100%";
-}
-
-function closePesquisa() {
-    document.getElementById("myPesquisa").style.width = "0%";
-};
-
