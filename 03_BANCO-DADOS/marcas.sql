503ea3806edb18b9a4305a5bfc9e6726308c77ea
Afonso Barbosa <afonso33barbosa@gmail.com>
2019-02-13 19:49:30 -0300

    adicionado sql de marcas na pasta 03

---
+++ 03_BANCO-DADOS/marcas.sql
@@ -0,0 +1,13 @@
+insert into marca (desc_marca) values ('Ford');
+insert into marca (desc_marca) values ('Hyundai');
+insert into marca (desc_marca) values ('Iveco');
+insert into marca (desc_marca) values ('Mercedes-Benz');
+insert into marca (desc_marca) values ('Scania');
+insert into marca (desc_marca) values ('Stralis');
+insert into marca (desc_marca) values ('Tector');
+insert into marca (desc_marca) values ('Volto');
+insert into marca (desc_marca) values ('Volkswagen');
+
+
+
+
